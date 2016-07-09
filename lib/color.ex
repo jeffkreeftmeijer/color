1bd4432c742051450c4b9becd3836b1b9ca59d58
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-07-09 15:26:32 +0200

    Add Color.green/1

--- lib/color.ex
+++ lib/color.ex
@@ -1,7 +1,13 @@
 defmodule Color do
   def red(color) do
-    [red | _ ] = Regex.scan(~r/../, color) |> List.flatten
+    [red, _, _] = Regex.scan(~r/../, color) |> List.flatten
     {base, _} = red |> Integer.parse(16)
+    base / 255
+  end
+
+  def green(color) do
+    [_, green, _ ] = Regex.scan(~r/../, color) |> List.flatten
+    {base, _} = green |> Integer.parse(16)
     base / 255
   end
 end
--- test/color_test.exs
+++ test/color_test.exs
@@ -7,4 +7,10 @@ defmodule ColorTest do
     assert Color.red("FF0000") == 1
     assert Color.red("0F0000") == 0.058823529411764705
   end
+
+  test "returns the green value for a color" do
+    assert Color.green("000000") == 0
+    assert Color.green("00FF00") == 1
+    assert Color.green("000F00") == 0.058823529411764705
+  end
 end
