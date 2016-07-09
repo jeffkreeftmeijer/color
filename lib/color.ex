58dc03d07213ab505eb632402ff932e5856f0c79
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-07-09 15:24:46 +0200

    Add Color.red/1

--- lib/color.ex
+++ lib/color.ex
@@ -1,2 +1,7 @@
 defmodule Color do
+  def red(color) do
+    [red | _ ] = Regex.scan(~r/../, color) |> List.flatten
+    {base, _} = red |> Integer.parse(16)
+    base / 255
+  end
 end
--- test/color_test.exs
+++ test/color_test.exs
@@ -2,7 +2,9 @@ defmodule ColorTest do
   use ExUnit.Case
   doctest Color
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "returns the red value for a color" do
+    assert Color.red("000000") == 0
+    assert Color.red("FF0000") == 1
+    assert Color.red("0F0000") == 0.058823529411764705
   end
 end
